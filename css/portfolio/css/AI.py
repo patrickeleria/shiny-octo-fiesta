24eaa2e2adc480fe8888dba367b54f8bfa511526
patrickeleria <73999608+patrickeleria@users.noreply.github.com>
2021-10-14 18:01:23 +0800

    MyPortfolioWebsite
    this portfolio website serves how I expand my skills in HTML and also CSS.

---
+++ css/portfolio/css/AI.py
@@ -0,0 +1,20 @@
+import random
+
+class Agent (object):
+    def __init__(self,env):
+
+        self.env=env
+
+    def go(self, n);
+    raise NotImplementedError("go")
+
+
+from display import Displayable:
+class Environment (Displayable):
+    def initiial percepts(self):
+
+        raise NotImplementedError("initial_percepts")
+
+        def do(self, action):
+
+            raise NotImplementedError("do")
